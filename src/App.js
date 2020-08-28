e471237add99b338502a9b9b615be945ec99b136
Freddy Munive <munivef@yahoo.com>
2020-08-27 20:59:32 -0500

    fixing some errors in yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import ThemeProvider from "@chakra-ui/core/dist/ThemeProvider";
-import {CSSReset} from '@chakra-ui/core'
-import theme from './theme'
-import PropertyDetails from './features/PropertyDetails/PropertyDetails'
+import { CSSReset } from "@chakra-ui/core";
+import theme from "./theme";
+import PropertyDetails from "./features/PropertyDetails/PropertyDetails";
 import { Container, MainApp } from "./sytles/general";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import PropertiesPage from "./features/pages/PropertiesPage";
 import Footer from "./features/shared/Footer";
 import Header from "./features/shared/Header";
@@ -13,27 +18,25 @@ import Home from "./features/pages/Home";
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>
-        <CSSReset/>
-    <MainApp>
-      <Router>
-        <Container>
-          <Header />
-        </Container>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <MainApp>
+        <Router>
+          <Container>
+            <Header />
+          </Container>
           <Switch>
-            <Route path="/" exact component={Home}></Route>
-              <Redirect to="/" />
-            </Route>
+            <Route path="/" exact component={Home} />
             <Container>
               <Route path="/properties" exact component={PropertiesPage} />
               <Route path="/properties/:id" component={PropertyDetails} />
-              <Route path="*" render={() => <div>page not found</div>} />
+              <Route path="*" render={() => <Home />} />
             </Container>
           </Switch>
-        <Footer />
-      </Router>
-    </MainApp>
-      </ThemeProvider>
+          <Footer />
+        </Router>
+      </MainApp>
+    </ThemeProvider>
   );
 }
 
